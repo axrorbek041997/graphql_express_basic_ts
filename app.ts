64f5eed09190cf901c2aa3d98805902c8ab6b9a9
pc <axrorbekbebitovich97@gmail.com>
2025-01-29 18:33:54 +0500

    fix: express types

--- app.ts
+++ app.ts
@@ -8,23 +8,23 @@ import authModule from './modules/auth/index.js'
 import authMiddleware from './utils/middlewares/auth.js'
 
 async function startServer() {
-    const app = express();
-    app.use(authMiddleware);
-    app.use(cors(), express.json());
-    
-    const httpServer = http.createServer(app);
-    
-    // Set up Apollo Server
-    const server = new ApolloServer({
-      typeDefs: [authModule.typeDefs],
-      resolvers: [authModule.resolvers],
-      plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-    });
-    await server.start();
+  const app = express();
+  app.use(authMiddleware);
+  app.use(cors(), express.json());
 
-    // @ts-ignore
-    app.use(expressMiddleware(server))
-    
-    httpServer.listen({ port: 4000 }, () => console.log(`🚀 Server ready at http://localhost:4000`))       
+  const httpServer = http.createServer(app);
+
+  // Set up Apollo Server
+  const server = new ApolloServer({
+    typeDefs: [authModule.typeDefs],
+    resolvers: [authModule.resolvers],
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await server.start();
+
+  // @ts-ignore
+  app.use(expressMiddleware(server))
+
+  httpServer.listen({ port: 4000 }, () => console.log(`🚀 Server ready at 4000 port`))
 }
 startServer()
